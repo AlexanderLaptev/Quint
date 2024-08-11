3948c4d36ef6274de8d6ed9529c7cb2a6bbb403f
Alexander Laptev <alexander_laptev4@inbox.ru>
2024-08-11 18:13:39 +0300

    add AudioWriter

---
+++ src/main/kotlin/lib/quint/AudioWriter.kt
@@ -0,0 +1,56 @@
+package lib.quint
+
+import lib.quint.source.AudioSource
+import lib.quint.source.MonoAudioSource
+import lib.quint.source.StereoAudioSource
+import lib.quint.util.SampleWriter
+import java.nio.ByteBuffer
+import javax.sound.sampled.AudioFormat
+
+object AudioWriter {
+    fun generate(
+        source: AudioSource,
+        buffer: ByteBuffer,
+        format: AudioFormat,
+        seconds: Double,
+        timeShift: Double = 0.0,
+    ) {
+        generate(source, buffer, format, (seconds * format.sampleRate).toInt(), timeShift)
+    }
+
+    fun generate(
+        source: AudioSource,
+        buffer: ByteBuffer,
+        format: AudioFormat,
+        frames: Int,
+        timeShift: Double = 0.0,
+    ) {
+        check(buffer.remaining() >= frames * format.frameSize) {
+            "Buffer cannot fit the specified number of frames"
+        }
+
+        val secondsPerFrame = 1.0 / format.sampleRate
+        var generatedFrames = 0L
+
+        while (generatedFrames < frames) {
+            val time = timeShift + generatedFrames.toDouble() * secondsPerFrame
+            when (source) {
+                is MonoAudioSource -> {
+                    val sample = source.sample(time)
+                    SampleWriter.writeSample(buffer, format, sample)
+                }
+
+                is StereoAudioSource -> {
+                    val sampleLeft = source.sampleLeft(time)
+                    val sampleRight = source.sampleRight(time)
+                    SampleWriter.writeSample(buffer, format, sampleLeft)
+                    SampleWriter.writeSample(buffer, format, sampleRight)
+                }
+            }
+            generatedFrames++
+        }
+    }
+
+    fun allocateBuffer(format: AudioFormat, frames: Int) =
+        ByteBuffer.allocate(frames * format.frameSize)
+}
