8118b578dcc2b91e5a2c02a5aa325795649d7580
Alexander Laptev <alexander_laptev4@inbox.ru>
2024-08-12 18:56:40 +0300

    open LowFrequencyOscillator

--- src/main/kotlin/lib/quint/synthesizer/LowFrequencyOscillator.kt
+++ src/main/kotlin/lib/quint/synthesizer/LowFrequencyOscillator.kt
@@ -2,7 +2,7 @@ package lib.quint.synthesizer
 
 import lib.quint.generator.Generator
 
-class LowFrequencyOscillator(
+open class LowFrequencyOscillator(
     var oscillator: Generator,
     var frequency: Double,
     var amplitude: Double,
@@ -10,7 +10,7 @@ class LowFrequencyOscillator(
     var attack: Double = 0.0,
     var delay: Double = 0.0,
 ) {
-    fun getValue(time: Double): Double {
+    open fun getValue(time: Double): Double {
         if (time < delay) return 1.0
         val afterDelay = time - delay
         val attackFactor = if (afterDelay < attack) afterDelay / attack else 1.0
